f5986794dfde22377476af986c3b073ee4b1b109
chitra <chitrakumari1302@gmail.com>
2024-07-22 22:52:03 +0530

    frontend changes for homepage

--- orgproject/marketplace/admin.py
+++ orgproject/marketplace/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from orgproject.marketplace.models import Product
+from orgproject.marketplace.models import Product, UserDetails
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(UserDetails)
