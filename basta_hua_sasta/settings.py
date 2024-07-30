1c512d2dc4fbf8318054bd679b03d094970e139c
Anshul Jain <anshul.jain1@spinny.com>
2024-07-30 20:35:15 +0530

    Runnable

--- basta_hua_sasta/settings.py
+++ basta_hua_sasta/settings.py
@@ -60,7 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 ROOT_URLCONF = 'basta_hua_sasta.urls'
 
